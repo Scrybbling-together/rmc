be858ec25aca601f576365751e7e92fd20f9be95
Laura Brekelmans <mail@laura.fm>
2025-04-19 23:32:20 +0200

    Update deps

--- flake.nix
+++ flake.nix
@@ -6,25 +6,15 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system: 
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
-        pythonEnv = pkgs.python312.withPackages (ps: []);
-        pkgs = import nixpkgs {
-          inherit system;
-        };
-      in
-      {
+        pkgs = import nixpkgs { inherit system; };
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-	    sqlite.dev
-	    poetry
-	    inkscape
-          ];
+          buildInputs = with pkgs; [ sqlite.dev poetry inkscape ];
 
-	  LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
-	    pkgs.sqlite.out
-	  ];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [ pkgs.sqlite.out ];
         };
       });
 }
